adaa368fe892161d6becbcfae8876695b8a9ee50
Siva1436 <nathansiva96@gmail.com>
2019-03-30 22:22:54 +0530

    Asian Commit in gitlearn1

--- src/test/java/PracAcademy/RestAPITest.java
+++ src/test/java/PracAcademy/RestAPITest.java
@@ -8,6 +8,9 @@ public class RestAPITest {
 	public void postjira()
 	{
 		System.out.println("postjira");
+		System.out.println("postjira1");
+		System.out.println("postjira2");
+		System.out.println("postjira3");
 	}
 
 	@Test
